8721809d6c4193823ec35a9496f80fecbcecaa10
mohit255 <mohitchack255@gmail.com>
2021-08-03 12:15:08 +0530

    Chat - 03/08/2021

--- node/index.js
+++ node/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
@@ -14,8 +14,15 @@ io.on('connection', (socket) => {
 console.log("user connected");
   socket.on('new_message', (data) => {
     console.log(data);
-		socket.broadcast.emit('new message', {"message":data.message,"from":data.from});
+    socket.user = data.from;
+		socket.broadcast.emit('new message', {"message":data.message,"from":socket.user});
 	
+  });
+
+  socket.on('useryping', (data) => {
+    // console.log(socket.user);
+    socket.broadcast.emit('useryping', {"user":socket.user});
+  
   });
 
 });
